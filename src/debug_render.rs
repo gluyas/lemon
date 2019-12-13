64712c0592a3c4c39a8c31342006710b41cc41d8
Marc Gluyas <marc.gluyas@gmail.com>
2019-12-13 14:12:07 +1300

    Add push_transform and pop_transform to DebugRender

--- src/debug_render.rs
+++ src/debug_render.rs
@@ -10,15 +10,18 @@ struct DrawCmd {
 
 #[derive(Debug)]
 pub struct DebugRender {
-    program:    GLuint,
-    u_camera:   GLint,
-    a_color:    GLuint,
-    a_position: GLuint,
+    program:     GLuint,
+    u_camera:    GLint,
+    a_color:     GLuint,
+    a_position:  GLuint,
+    a_transform: GLuint,
 
     camera: Camera,
 
     reverse_index: usize,
     draw_cmds: Vec<DrawCmd>,
+
+    transform_stack: Vec<Mat4>,
 }
 
 impl DebugRender {
@@ -35,21 +38,30 @@ impl DebugRender {
         ]).unwrap();
         let u_camera = gl::get_uniform_location(program, cstr!("u_camera")).unwrap();
 
-        let a_color    = gl::get_attrib_location(program, cstr!("a_color")).unwrap();
-        let a_position = gl::get_attrib_location(program, cstr!("a_position")).unwrap();
+        let a_color     = gl::get_attrib_location(program, cstr!("a_color")).unwrap();
+        let a_position  = gl::get_attrib_location(program, cstr!("a_position")).unwrap();
+        let a_transform = gl::get_attrib_location(program, cstr!("a_transform")).unwrap();
 
         DebugRender {
-            program, u_camera, a_color, a_position,
+            program, u_camera, a_color, a_position, a_transform,
             camera: Camera::default(),
             draw_cmds: Vec::new(), reverse_index: !0,
+            transform_stack: vec![Mat4::identity()],
         }
     }
 
     pub fn with_shared_context(share: &DebugRender) -> Self {
-        unsafe {
-            let mut new: DebugRender = mem::transmute_copy(share);
-            new.draw_cmds = Vec::new();
-            new
+        DebugRender {
+            program: share.program, 
+            u_camera: share.u_camera,
+            a_color: share.a_color, 
+            a_position: share.a_position,
+            a_transform: share.a_transform,
+            camera: share.camera,
+
+            draw_cmds: Vec::new(),
+            reverse_index: !0,
+            transform_stack: vec![Mat4::identity()],
         }
     }
 
@@ -87,22 +99,38 @@ impl DebugRender {
 
             draw_cmd.vbo = gl::gen_object(gl::GenBuffers);
             gl::BindBuffer(gl::ARRAY_BUFFER, draw_cmd.vbo);
-            gl::buffer_init::<Vec3>(gl::ARRAY_BUFFER, points.len() + 1, gl::STATIC_DRAW);
 
-            gl::buffer_sub_data(gl::ARRAY_BUFFER, 0, slice::from_ref(color));
-            gl::EnableVertexAttribArray(self.a_color);
-            gl::VertexAttribPointer(
-                self.a_color, 3, gl::FLOAT, gl::FALSE,
-                0, ptr::null::<Vec3>().offset(0) as *const GLvoid
-            );
-            gl::VertexAttribDivisor(self.a_color, 1);
+            gl::buffer_init::<f32>(gl::ARRAY_BUFFER, 3*points.len() + 3 + 16, gl::STATIC_DRAW);
 
-            gl::buffer_sub_data(gl::ARRAY_BUFFER, 1, points);
+            gl::buffer_sub_data(gl::ARRAY_BUFFER, 0, points);
             gl::EnableVertexAttribArray(self.a_position);
             gl::VertexAttribPointer(
                 self.a_position, 3, gl::FLOAT, gl::FALSE,
-                0, ptr::null::<Vec3>().offset(1) as *const GLvoid
+                0, ptr::null::<Vec3>().offset(0) as *const GLvoid,
             );
+
+            gl::buffer_sub_data(gl::ARRAY_BUFFER, points.len(), slice::from_ref(color));
+            gl::EnableVertexAttribArray(self.a_color);
+            gl::VertexAttribPointer(
+                self.a_color, 3, gl::FLOAT, gl::FALSE,
+                0, ptr::null::<Vec3>().offset(points.len() as isize) as *const GLvoid
+            );
+            gl::VertexAttribDivisor(self.a_color, 1);
+
+            let transform = self.transform_stack.last().unwrap();
+            gl::buffer_sub_data::<f32>(gl::ARRAY_BUFFER, 
+                3*points.len() + 3, 
+                AsRef::<[f32; 16]>::as_ref(&transform)
+            );
+            for i in 0..4 {
+                let a_transform_i = self.a_transform + i as GLuint;
+                gl::EnableVertexAttribArray(a_transform_i);
+                gl::VertexAttribPointer(
+                    a_transform_i, 4, gl::FLOAT, gl::FALSE,
+                    0, ptr::null::<f32>().offset(3*points.len() as isize + 3 + 4*i) as *const GLvoid
+                );
+                gl::VertexAttribDivisor(a_transform_i, 1);
+            }
 
             self.draw_cmds.push(draw_cmd);
         }); }
@@ -123,13 +151,15 @@ impl DebugRender {
             draw_cmd.vbo = gl::gen_object(gl::GenBuffers);
             gl::BindBuffer(gl::ARRAY_BUFFER, draw_cmd.vbo);
 
-            let origin    = transform.transform_point(point3!()).to_vec();
-            let transform = |v: Vec3| transform.transform_point(point3!(scale*v)).to_vec();
-            gl::buffer_data(gl::ARRAY_BUFFER, &[
-                VEC3_X, transform(VEC3_X), VEC3_X, origin,
-                VEC3_Y, transform(VEC3_Y), VEC3_Y, origin,
-                VEC3_Z, transform(VEC3_Z), VEC3_Z, origin,
-            ], gl::STATIC_DRAW);
+            const AXES: &[Vec3] = &[
+                VEC3_X, VEC3_X, VEC3_X, VEC3_0,
+                VEC3_Y, VEC3_Y, VEC3_Y, VEC3_0,
+                VEC3_Z, VEC3_Z, VEC3_Z, VEC3_0,
+            ];
+
+            // TODO: use a shared array buffer
+            gl::buffer_init::<f32>(gl::ARRAY_BUFFER, 3*AXES.len() + 16, gl::STATIC_DRAW);
+            gl::buffer_sub_data(gl::ARRAY_BUFFER, 0, AXES);
 
             gl::EnableVertexAttribArray(self.a_color);
             gl::VertexAttribPointer(
@@ -144,6 +174,23 @@ impl DebugRender {
                 mem::size_of::<[Vec3; 2]>() as GLsizei,
                 mem::size_of::<[Vec3; 1]>() as *const GLvoid
             );
+
+            let transform = self.transform_stack.last().unwrap() 
+                          * transform 
+                          * Mat4::from_diagonal(vec4!(scale*VEC3_1, 1.0));
+            gl::buffer_sub_data::<f32>(gl::ARRAY_BUFFER, 
+                3*AXES.len(), 
+                AsRef::<[f32; 16]>::as_ref(&transform)
+            );
+            for i in 0..4 {
+                let a_transform_i = self.a_transform + i as GLuint;
+                gl::EnableVertexAttribArray(a_transform_i);
+                gl::VertexAttribPointer(
+                    a_transform_i, 4, gl::FLOAT, gl::FALSE,
+                    0, ptr::null::<f32>().offset(3*AXES.len() as isize + 4*i) as *const GLvoid
+                );
+                gl::VertexAttribDivisor(a_transform_i, 1);
+            }
 
             self.draw_cmds.push(draw_cmd);
         }); }
@@ -165,9 +212,23 @@ impl DebugRender {
         self.reverse_index = !0;
     }
 
+    pub fn push_transform(&mut self, transform: &Mat4) {
+        self.transform_stack.push(self.transform_stack.last().unwrap() * transform);
+    }
+
+    pub fn pop_transform(&mut self) {
+        if self.transform_stack.len() <= 1 { 
+            panic!("DebugRender attempted to pop empty transform stack"); 
+        }
+        self.transform_stack.pop();
+    }
+
     pub fn render_frame(&mut self) {
         if self.reverse_index != !0 {
             panic!("DebugRender attemped render_frame without ending reversed sequence");
+        }
+        if self.transform_stack.len() != 1 {
+            panic!("DebugRender pushed a transform that was not popped");
         }
         if self.draw_cmds.is_empty() { return; }
         unsafe { gl_with_temp_state!(
--- src/shader/debug.vert.glsl
+++ src/shader/debug.vert.glsl
@@ -2,6 +2,7 @@
 
 uniform mat4 u_camera;
 
+in mat4 a_transform;
 in vec3 a_position;
 in vec3 a_color;
 
@@ -11,5 +12,5 @@ out vec4 gl_Position;
 
 void main() {
     v_color = a_color;
-    gl_Position = u_camera * vec4(a_position, 1.0);
+    gl_Position = u_camera * a_transform * vec4(a_position, 1.0);
 }
